f36b35700aaa46c16af4257716c0f211a8827d0e
huangjing <260741887@qq.com>
2018-03-03 09:04:20 +0800

    ccccc

--- ajax/test.php
+++ ajax/test.php
@@ -1,6 +1,21 @@
 <?php
 
-$arr = [ 'name'=>'eric',  'age'=>35, 'city'=>"深圳"];
+extract($_REQUEST);
 
-echo json_encode($arr);
+
+if($_SERVER['REQUEST_METHOD']=='POST') {
+
+	$arr[] = [ 'name'=>'eric',  'age'=>35, 'city'=>"深圳"];
+	$arr[] = [ 'name'=>'ashley',  'age'=>24, 'city'=>"上海"];
+	$arr[] = [ 'name'=>$a,  'age'=>$b, 'city'=>"Paris"];
+	echo json_encode($arr);
+
+} else {
+
+	$arr[] = [ 'emma', 11, "桂林"];
+	$arr[] = [ 'mia', 33,  "北京"];
+	$arr[] = [ $a,  "$b", "New York"];
+	echo json_encode($arr);
+
+}
 
